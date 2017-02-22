bf0a6e95cfeb23384e6f25feb6d3e510612c5d24
MarcusNilsson <97marcusnilsson@gmail.com>
2017-02-22 16:26:39 +0100

    Add files via upload
    Kan nå legge inn variabler i html filen

--- app/views.py
+++ app/views.py
@@ -5,4 +5,12 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    tittel = {'tittel1': 'Fotball', 'tittel2': 'Håndball'}
+    startTid = {'startTid1': '08:30'}
+    sluttTid = {'sluttTid1': '09:30'}
+    id = {'id1': '1'}
+    return render_template("index.html",
+                           tittel=tittel,
+                           startTid=startTid,
+                           sluttTid=sluttTid,
+                           id=id)
