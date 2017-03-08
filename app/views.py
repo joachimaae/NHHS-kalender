4b89a0db6002c47d6cf36eb5165f7084e0e92d11
MarcusNilsson <97marcusnilsson@gmail.com>
2017-03-08 15:49:52 +0100

    Add files via upload
    ny versjon 08.03

--- app/views.py
+++ app/views.py
@@ -2,13 +2,24 @@ from flask import render_template
 from app import app
 
 
-@app.route('/')
 @app.route('/index')
+
 def index():
-    tittel = {'tittel1': 'Fotball', 'tittel2': 'Håndball'}
-    startTid = {'startTid1': '08:30'}
-    sluttTid = {'sluttTid1': '09:30'}
-    id = {'id1': '1'}
+    fakeTittel = {'tittel1': 'Fotball', 'tittel2': 'Håndball'}
+    fakeStartTid = {'startTid1': '08:30','startTid2': '09:30'}
+    fakeSluttTid = {'sluttTid1': '08:30','sluttTid2': '09:30'}
+    fakeId = {'id1': '1','id2': '2'}
+
+    tittel = {}
+    startTid = {}
+    sluttTid = {}
+    id = {}
+    for x in range(0, len(fakeTittel) - 1):
+        tittel.update(fakeTittel[x])
+        startTid.update(fakeStartTid[x])
+        sluttTid.update(fakeSluttTid[x])
+        id.update(fakeId[x])
+
     return render_template("index.html",
                            tittel=tittel,
                            startTid=startTid,
