052a5f159cfa6310d48df510dce8a51b460489d5
joachimaae <aae.joachim@gmail.com>
2018-12-02 22:27:09 +0100

    Ukebytter

--- app/views.py
+++ app/views.py
@@ -1,12 +1,18 @@
 from flask import render_template
 from app import app
 from app import gcal
-
+import datetime
 @app.route('/index')
 @app.route('/')
 def index():
     events = gcal.hent_events()
-    return render_template("index.html", eventer=events)
+    ukenr = datetime.datetime.now().isocalendar()[1]
+    return render_template("index.html", eventer=events, ukenummer_dag=int(ukenr))
+
+@app.route('/index/<weeknum>')
+def weekswitch(weeknum):
+    events = gcal.hent_events()
+    return render_template("index.html", eventer=events, ukenummer_dag=int(weeknum))
 
 @app.route('/liste')
 def liste():
